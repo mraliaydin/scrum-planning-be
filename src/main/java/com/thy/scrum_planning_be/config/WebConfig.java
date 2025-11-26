4fc332accea7da9d9fc7a955ba50e398cb7330f2
mraliaydin <aaydn46@gmail.com>
2025-11-26 11:09:08 +0300

    Initial commit

--- src/main/java/com/thy/scrum_planning_be/config/WebConfig.java
+++ src/main/java/com/thy/scrum_planning_be/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://aliaydin.me")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
-                // .allowCredentials(true);
+                .allowedHeaders("*")
+                .allowCredentials(true); // WebSocket için gerekli
     }
 }
