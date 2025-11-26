ba65819ab193d22c590b2bb81dc9ff2cdc600291
mraliaydin <aaydn46@gmail.com>
2025-11-26 10:50:29 +0300

    Initial commit

--- src/main/java/com/thy/scrum_planning_be/config/WebConfig.java
+++ src/main/java/com/thy/scrum_planning_be/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
+                // .allowCredentials(true);
     }
 }
